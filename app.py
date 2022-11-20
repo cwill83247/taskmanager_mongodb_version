4c22a54bb031d01067cf7eab298523f0b418be23
cwill83247 <breakthrumusic@hotmail.com>
2022-11-20 15:06:27 +0000

    Add: Addign functionality to edit task

--- app.py
+++ app.py
@@ -116,21 +116,25 @@ def add_task():
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
-@app.route("/edit_task/<task_id>", methods=["GET", "POST"])            # need to pass in the_id from the mongoDb to ensure editing correct entry 
+
+@app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
-    if request.method == "POST":                                      #Post method required for the Edit Form 
+    if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
-            submit = {
-                "category_name": request.form.get("category_name"),
-                "task_name": request.form.get("task_name"),
-                "task_description": request.form.get("task_description"),
-                "is_urgent": is_urgent,
-                "due_date": request.form.get("due_date"),
-                "created_by": session["user"]
-        }  
+        submit = { "$set": {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            #"created_by": session["user"]
+        }}
+        mongo.db.tasks.update_one({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated")
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("edit_task.html", task=task, categories=categories)             #passign through the taks vairable above hwich is define the exact task by its ID
+    return render_template("edit_task.html", task=task, categories=categories)
 
 
 if __name__ == "__main__":                              #std using this to test we can access env.py and values
